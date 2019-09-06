a7690950175bc1629a409f636a1aa0eae06e9242
CarverFan <54744412+CarverFan@users.noreply.github.com>
2019-09-06 14:52:44 +0100

    Added record facility and included gaugelib.py
    App now has an extra camera control button to start and stop recording video.  Most of the changes are in rvcamlib.py.  I've included the required gaugelib.py (slightly modified from Ardiotech's version - hope this is OK to do?)

--- rvcamlib.py
+++ rvcamlib.py
@@ -2,24 +2,31 @@
 from time import time, sleep
 from tkinter import *
 from picamera import PiCamera
+from datetime import datetime, date, timedelta
 
 camera = PiCamera()
+
+vidpath = '/media/usb/video/'
 
 class rearViewCam:
 
     def __init__(self, master):
         self.master = master
+        self.button_clicks = 0
 
         self.labelText = StringVar()
-        self.labelText.set("Cam Status")
+        self.labelText.set("Cam Status Off")
         self.camStatus = Label(master, textvariable=self.labelText)
         self.camStatus.grid(row=2,columnspan=3)
 
-        self.startButton = Button(master, text="Start Cam", command=self.startCam)
-        self.startButton.grid(row=1,column=1)
+        self.startButton = Button(master, text="Start", command=self.startCam)
+        self.startButton.grid(row=1,column=0)
 
-        self.stopButton = Button(master, text="Stop Cam", state=DISABLED, command=self.stopCam)
-        self.stopButton.grid(row=1,column=2)
+        self.stopButton = Button(master, text="Stop", state=DISABLED, command=self.stopCam)
+        self.stopButton.grid(row=1,column=1)
+
+        self.recButton = Button(master, text="Record", fg="green", command=self.recCam)
+        self.recButton.grid(row=1,column=2)
 
     def startCam(self):
         self.labelText.set("Running")
@@ -27,13 +34,36 @@ class rearViewCam:
         
         camera.resolution = (1024, 768)
         camera.start_preview(fullscreen = False,
-                             window = (190,40,510,350))
+                             window = (185,25,510,350))
 
     def stopCam(self):
         if self.startCam is not None:
             self.startf = None
             camera.stop_preview()
-            self.labelText.set("Stopped")
- 
+            self.labelText.set("Stopped") 
+
+    def recCam(self):
+        self.button_clicks += 0
+        self.labelText.set("Recording ")
+        self.recButton['text'] = "Start"
+        if self.button_clicks == 0:
+            self.button_clicks += 1
+            self.recButton['text'] = "Started"
+            self.recButton['fg'] = "red"
+            # start recording
+            now = datetime.now()
+            dtStr = now.strftime("%d%m%Y-%H%M%S")
+            vidfile = vidpath + "vid-" + dtStr + ".h264"
+            camera.resolution = (1024, 768)
+            camera.start_recording(vidfile)
+        else:
+            self.labelText.set("Stopped Recording")
+            self.recButton['text'] = "Record"
+            self.recButton['fg'] = "green"
+            # stop recording
+            camera.stop_recording()
+            self.button_clicks = 0
+           
+
 
 
